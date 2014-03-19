fd0e934ba21268f32223e88d90514b06c8afd51e
Maxime Heckel <heckelmaxime@gmail.com>
2014-03-20 00:41:46 +0100

    add socket to route

--- server.js
+++ server.js
@@ -37,12 +37,14 @@ app.get('/ssh', function (req,res) {
 });
 
 app.get('/containers/:id',function(req,res){
+  io.sockets.on('connectionview', function(socket){
   socket.on('inspectId',function(data){
     var containerId=data;
     console.log('INSPECT CONTAINER WITH ID '+containerId);
     docker.containers.inspect(containerId,function(err,req){
       res.render('containers/show.ejs',{container: req});
     });
+  });
   });
 });
 
