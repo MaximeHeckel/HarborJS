1833d69ab72f30fe6dd8130c91d71d119787c15c
Maxime Heckel <heckelmaxime@gmail.com>
2014-03-22 18:12:55 +0100

    add new container view

--- app/routes.js
+++ app/routes.js
@@ -5,17 +5,17 @@ module.exports = function(app, passport) {
 
 // normal routes ===============================================================
 
-	// show the home page
-	app.get('/', function(req, res) {
-		res.render('index.ejs');
-	});
+  // show the home page
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
 
-	// PROFILE SECTION =========================
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user
-		});
-	});
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+      user : req.user
+    });
+  });
 
   app.get('/dashboard', isLoggedIn,  function (req,res) {
     docker.containers.list(function(err,cont){
@@ -37,13 +37,16 @@ module.exports = function(app, passport) {
   });
 
 
-	// LOGOUT ==============================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
 // app routes ===============================================================
+  app.get('/containers/new', function(req,res){
+    res.render('containers/new.ejs',{user : req.user});
+  });
 
   app.post( '/create', config.create );
 
@@ -52,60 +55,59 @@ module.exports = function(app, passport) {
 // AUTHENTICATE (FIRST LOGIN) ==================================================
 // =============================================================================
 
-	// locally --------------------------------
-		// LOGIN ===============================
-		// show the login form
-		app.get('/login', function(req, res) {
-			res.render('login.ejs', { message: req.flash('loginMessage') });
-		});
+  // locally --------------------------------
+    // LOGIN ===============================
+    // show the login form
+    app.get('/login', function(req, res) {
+      res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
 
-		// process the login form
-		app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+      successRedirect : '/profile', // redirect to the secure profile section
+      failureRedirect : '/login', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+}));
 
-		// SIGNUP =================================
-		// show the signup form
-		app.get('/signup', function(req, res) {
-			res.render('signup.ejs', { message: req.flash('signupMessage') });
-		});
+    // SIGNUP =================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+      res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
 
-		// process the signup form
-		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/signup', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+      successRedirect : '/profile', // redirect to the secure profile section
+      failureRedirect : '/signup', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+    }));
 
 
 
-	// locally --------------------------------
-		app.get('/connect/local', function(req, res) {
-			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-		});
-		app.post('/connect/local', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+  // locally --------------------------------
+    app.get('/connect/local', function(req, res) {
+      res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+    });
+    app.post('/connect/local', passport.authenticate('local-signup', {
+      successRedirect : '/profile', // redirect to the secure profile section
+      failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+    }));
 
-	// unlink -----------------------------------
-	app.get('/unlink/local', function(req, res) {
-		var user            = req.user;
-		user.local.username    = undefined;
-		user.local.password = undefined;
-		user.save(function(err) {
-			res.redirect('/');
-		});
-	});
+  // unlink -----------------------------------
+  app.get('/unlink/local', function(req, res) {
+    var user            = req.user;
+    user.local.username    = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/');
+    });
+  });
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
-		return next();
-
-	res.redirect('/');
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/');
 }
