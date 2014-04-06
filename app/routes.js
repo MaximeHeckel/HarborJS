30152a3b85a20bf180a9c1684573f73a49aa783b
MaximeHeckel <heckelmaxime@gmail.com>
2014-04-06 18:24:14 +0200

    translatation

--- app/routes.js
+++ app/routes.js
@@ -58,59 +58,6 @@ module.exports = function(app, passport) {
 
   app.get('/destroy/:id', config.destroy);
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
-
-  // locally --------------------------------
-    // LOGIN ===============================
-    // show the login form
-    app.get('/login', function(req, res) {
-      res.render('login.ejs', { message: req.flash('loginMessage') });
-    });
-
-    // process the login form
-    app.post('/login', passport.authenticate('local-login', {
-      successRedirect : '/profile', // redirect to the secure profile section
-      failureRedirect : '/login', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
-
-    // SIGNUP =================================
-    // show the signup form
-    app.get('/signup', function(req, res) {
-      res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
-
-    // process the signup form
-    app.post('/signup', passport.authenticate('local-signup', {
-      successRedirect : '/profile', // redirect to the secure profile section
-      failureRedirect : '/signup', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
-
-
-
-  // locally --------------------------------
-    app.get('/connect/local', function(req, res) {
-      res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-    });
-    app.post('/connect/local', passport.authenticate('local-signup', {
-      successRedirect : '/profile', // redirect to the secure profile section
-      failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
-
-  // unlink -----------------------------------
-  app.get('/unlink/local', function(req, res) {
-    var user            = req.user;
-    user.local.username    = undefined;
-    user.local.password = undefined;
-    user.save(function(err) {
-      res.redirect('/');
-    });
-  });
-
   //Oauth
   app.get('/oauth',
     passport.authenticate('oauth2'));
